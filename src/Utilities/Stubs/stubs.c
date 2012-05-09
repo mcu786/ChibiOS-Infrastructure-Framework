10144510022b780e3968ccf6a04b587dbe4c28ca
Matthias Blaicher <matthias@blaicher.com>
2012-05-09 18:12:26 +0200

    [fix] Better implementations of cxa guards and other stubs

--- src/Utilities/Stubs/stubs.c
+++ src/Utilities/Stubs/stubs.c
@@ -12,7 +12,6 @@
 
 #include "stubs.h"
 
-// FIXME: Stubs - HATE THEM
 void _exit(int status){
 	(void) status;
 	chSysHalt();
@@ -28,9 +27,29 @@ void _kill(pid_t id){
 	(void) id;
 }
 
-/* guard variables */
-int __cxa_guard_acquire(__guard *g) {return !*(char *)(g);}
-void __cxa_guard_release (__guard *g) {*(char *)g = 1;}
-void __cxa_guard_abort (__guard * g __attribute__((unused))) {}
-void __cxa_end_cleanup (void) {}
-void __cxa_pure_virtual(void) { while (1); }
+
+void __cxa_pure_virtual(void) {
+  chDbgPanic("__cxa_pure_virtual() #1");
+}
+
+
+int __cxa_guard_acquire(__guard *g) {
+  chSysLock();
+  const int result = !*(char *)(g);
+  chSysUnlock();
+
+  return result;
+}
+
+void __cxa_guard_release(__guard *g) {
+  chSysLock();
+  *(char *)g = 1;
+  chSysUnlock();
+}
+
+void __cxa_guard_abort(__guard *g) {
+  chSysLock();
+  *(char *)g = 1;
+  chSysUnlock();
+}
+
